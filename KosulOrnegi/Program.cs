0ae857df8c41f5f64d39cf7d530e3ceb51e04198
Aysun YILDIRIM <82328345+aysunyildirim@users.noreply.github.com>
2023-05-29 22:53:03 +0300

    Add project files.

---
+++ KosulOrnegi/Program.cs
@@ -0,0 +1,33 @@
+﻿
+namespace KosulOrnegi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+        Ornek1();
+            Console.ReadLine();
+        }
+        private static void Ornek1()
+        {
+            if (true)
+            {
+                Console.WriteLine("Bu kod her zaman calısır");
+            }
+            if (DateTime.Today.Day==28)
+            {
+                Console.WriteLine("Ayın 28 e geldi,indirim zamanı");
+
+            }
+            else if (DateTime.Today.Day==12)
+            {
+                Console.WriteLine("Çok az kaldı! Yarın indirim var.");
+            }
+            else
+            {
+                Console.WriteLine("İndirim için ayın 13 ünü bekleyin");
+            }
+
+        }
+    }
+}
